5878a640caa9ddc2571edd4bd8ac7e622f3e7705
fyang0507 <fredyang0507@gmail.com>
2026-01-24 20:36:49 -0500

    hotfix: typo on landing page.

--- src/components/landing/HowItWorks.tsx
+++ src/components/landing/HowItWorks.tsx
@@ -37,7 +37,7 @@ export const HowItWorks: React.FC = () => {
     <section className="pt-12 pb-24 px-6 relative z-10">
       <div className="max-w-7xl mx-auto">
         <div className="mb-16 text-center">
-          <h2 className="text-3xl md:text-4xl font-bold mb-4">Never waste an exprience. </h2>
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">Never waste an experience. </h2>
           <p className="text-zinc-400 max-w-2xl mx-auto">
             tsugi enables an agent to memorize procedures, not facts.
           </p>
